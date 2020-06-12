1dde64d2cd937cc7c962f1489196dee763dd56a0
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-12 14:12:32 +0300

    6. Изменить программу Tracker из 2-го модуля[10039#228836]

--- src/main/java/tracker/Tracker.java
+++ src/main/java/tracker/Tracker.java
@@ -6,29 +6,25 @@ import static java.util.List.copyOf;
 
 public class Tracker {
 
-    private final ArrayList<Item> items = new ArrayList<>();
+    private final List<Item> items = new ArrayList<>();
     //Item[] items = new Item[100];
     //private int positions = 0;
-
     private String generateId() {
         Random rd = new Random();
         return String.valueOf(rd.nextLong() + System.currentTimeMillis());
     }
-
     public Item add(Item item) {
         item.setId(generateId());
         //items[positions++] = item;
         return item;
     }
-
     public List<Item> findAll() {
         //ArrayList cloneItem = (ArrayList) items.clone();
         return items;
         //copyOf(items);
         //ArraysList<Item>.copyOf(items, positions);
     }
-
-    public ArrayList<Item> findByName(String key) {
+    public List<Item> findByName(String key) {
         ArrayList<Item> newItemsArray = new ArrayList<>();
         //Item[] newItemsArray = new Item[positions];
         int newSize = 0;
@@ -47,12 +43,10 @@ public class Tracker {
         //return ArrayList<Item>.copyOf(newItemsArray);
         //Arrays.copyOf(newItemsArray, newSize);
     }
-
     public Item findById(String id) {
         int index = indexOf(id);
         return index != -1 ? items.get(index) : null;
     }
-
     public boolean replase(String id, Item item) {
         boolean result = false;
         int index = indexOf(id);
@@ -74,7 +68,6 @@ public class Tracker {
             }
         } return rls;
     }
-
     private int indexOf(String id) {
         int rls = -1;
 //        for (Item x : items
@@ -96,7 +89,6 @@ public class Tracker {
 //                break;
         return rls;
     }
-
     public boolean delete(String id) {
         boolean result = false;
         int index = indexOf(id);
