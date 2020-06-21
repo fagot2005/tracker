bcbd45faf86328e358f4cb3c490144088970b481
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-21 17:54:06 +0300

    1. Фильтрация учеников. [#228697]

--- src/main/java/stream/School.java
+++ src/main/java/stream/School.java
@@ -5,12 +5,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    public  List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> collect10A = students.stream().filter(student -> student.getScore()>=70).collect(Collectors.toList());
-        collect10A.forEach(System.out::println);
-                List<Student> collect10B = students.stream().filter(student -> student.getScore()>=50 && student.getScore()<70).collect(Collectors.toList());
-        collect10B.forEach(System.out::println);
-                List<Student> collect10V = students.stream().filter(student -> student.getScore()<50).collect(Collectors.toList());
-        collect10V.forEach(System.out::println);
+    public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream().filter(predict).collect(Collectors.toList());
     }
 }
--- src/test/java/stream/SchoolTest.java
+++ src/test/java/stream/SchoolTest.java
@@ -1,12 +1,19 @@
 package stream;
 
-import junit.framework.TestCase;
 
+
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class SchoolTest extends TestCase {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
-    public void testCollect() {
+public class SchoolTest{
+
+    @Test
+    public void class10A() {
         List<Student> students = List.of(
                 new Student(40, "Ivanov"),
                 new Student(50, "Ivanovich"),
@@ -16,6 +23,49 @@ public class SchoolTest extends TestCase {
                 new Student(90, "Karpov"),
                 new Student(95, "Smirnov"),
                 new Student(45, "Catov"));
-        School.collect(students, x -> students.getScore() < 50);
+        List<Student> resalt = School.collect(students, x -> x.getScore() >= 70);
+        for (Student res: resalt
+        ) {
+            System.out.println(res.getScore() + ", " + res.getSureName());
+        }
+        assertThat(resalt.size(), is(4));
+    }
+
+    @Test
+    public void class10B() {
+        List<Student> students = List.of(
+                new Student(40, "Ivanov"),
+                new Student(50, "Ivanovich"),
+                new Student(60, "Petrov"),
+                new Student(70, "Petrenko"),
+                new Student(80, "Sidorov"),
+                new Student(90, "Karpov"),
+                new Student(95, "Smirnov"),
+                new Student(45, "Catov"));
+        List<Student> resalt = School.collect(students, x -> x.getScore() >= 50 && x.getScore() < 70);
+        for (Student res: resalt
+        ) {
+            System.out.println(res.getScore() + ", " + res.getSureName());
+        }
+        assertThat(resalt.size(), is(2));
+    }
+
+    @Test
+    public void class10V() {
+        List<Student> students = List.of(
+                new Student(40, "Ivanov"),
+                new Student(50, "Ivanovich"),
+                new Student(60, "Petrov"),
+                new Student(70, "Petrenko"),
+                new Student(80, "Sidorov"),
+                new Student(90, "Karpov"),
+                new Student(95, "Smirnov"),
+                new Student(45, "Catov"));
+        List<Student> resalt = School.collect(students, x -> x.getScore() >= 70);
+        for (Student res: resalt
+        ) {
+            System.out.println(res.getScore() + ", " + res.getSureName());
+        }
+        assertThat(resalt.size(), is(4));
     }
 }
