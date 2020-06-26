dcc7487d2e8d65db8910d4392a6d7616cee41c7f
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-26 22:46:31 +0300

    4. Преобразование List в Map. [#228700]

--- src/main/java/stream/StudentLevel.java
+++ src/main/java/stream/StudentLevel.java
@@ -2,15 +2,17 @@ package stream;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StudentLevel {
-    public static List<Student> levelOf(List<Student> students, int bound) {
+    public static Map<String, Student> levelOf(List<Student> students, int bound) {
         return students.stream()
                 .flatMap(Stream::ofNullable)
                 .sorted((left, right) -> left.getSureName().compareTo(right.getSureName()))
                 .takeWhile(student -> student.getScore() > bound)
-                .collect(Collectors.toList());
+                .collect(Collectors.toMap(student -> student.getSureName(), student -> student));
+                  //.collect(Collectors.toList());
     }
 }
