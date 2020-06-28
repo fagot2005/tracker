9668b1e7e921e1b827215552268cc51de9a592b9
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-28 17:16:03 +0300

    5. Преобразование матрицы чисел в список чисел. [#228701]

--- src/test/java/stream/MatrixToListTest.java
+++ src/test/java/stream/MatrixToListTest.java
@@ -24,7 +24,7 @@ public class MatrixToListTest {
     public void intMatrixToList() {
         Integer[][] matrix = new Integer[][]{{1, 2, 3, 4, 5},{ 6, 7, 8, 9, 10}};
         List<Integer> matrixToList = MatrixToList.matrixStream(matrix);
-        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         assertThat(matrixToList, is(expected));
     }
 }
