aab3c0f9ea3207ec88083b9fe4b3069c33e42c8f
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-08-07 13:50:34 +0300

    1. Sql схема для проекта Tracker. [#313721]

--- src/main/java/db/filters.sql
+++ src/main/java/db/filters.sql
@@ -20,24 +20,27 @@
 
 -- insert into products
 -- values
--- (1, 'Parmezan', 2, '2020-09-20', 250.0, 250),
--- (2, 'Blue Cheeses', 2, '2020-10-20', 400.0, 80),
--- (3, 'Rokfor', 2, '2020-09-12', 600.0, 10),
--- (4, 'Dar Blue', 2, '2020-10-16', 80.0, 320),
--- (5, 'Bonakva', 1, '2020-12-16', 5.0, 1200),
--- (6, 'Tetra Pack', 3, '2020-09-09', 12.0, 100),
--- (7, 'Parmalat', 3, '2020-09-24', 28.0, 480),
--- (8, 'Nestle', 3, '2020-11-20', 40.0, 850);
+-- (1, 'Parmezan', 2, '2020-09-20', 250.0, 3),
+-- (2, 'Blue Cheeses', 2, '2020-10-20', 400.0, 2),
+-- (3, 'Rokfor', 2, '2020-09-12', 600.0, 1),
+-- (4, 'Dar Blue', 2, '2020-10-16', 80.0, 2),
+-- (5, 'Bonakva', 1, '2020-12-16', 5.0, 1),
+-- (6, 'Tetra Pack', 3, '2020-09-09', 12.0, 1),
+-- (7, 'Parmalat', 3, '2020-09-24', 28.0, 1),
+-- (8, 'Nestle', 3, '2020-11-20', 40.0, 1),
+-- (9, 'Ice cream Magnum', 3, '2020-11-20', 40.0, 2),
+-- (10, 'Ice cream Cornetto', 2, '2020-11-20', 25.0, 2);
+
 
 -- select * from products
 -- inner join tipe t on products.tipe_id = t.id
 -- where t.name = 'Cheese'
 
 -- select * from products
--- where name like '%ar%'
+-- where lower (name) like  lower ('%ice cream%');
 
 -- select * from products
--- where expired_date >='2020-09-01' and expired_date <='2020-09-30'
+-- where EXTRACT(month from expired_date) = EXTRACT(month from current_date) + 1
 
 -- select *    from products
 -- where prise = (select MAX(prise) from products)
@@ -50,9 +53,10 @@
 -- inner join tipe t on products.tipe_id = t.id
 -- where t.name = 'Milk' or t.name = 'Cheese'
 
--- select * from products
--- inner join tipe t on products.tipe_id = t.id
--- where in_stock<50
+-- select tipe.id, tipe.name,  Sum(products.in_stock) as stock from tipe
+-- left join products on tipe.id = products.tipe_id
+-- group by tipe.name, tipe.id
+-- having Sum(products.in_stock) < 10
 
 -- select products.id, products.name, t.name from products
 -- inner join tipe t on products.tipe_id = t.id
