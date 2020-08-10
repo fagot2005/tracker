c3198096d837a31086adb41c687d05af019a2dbe
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-08-10 23:23:27 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy. [#313719]

--- src/main/java/tracker/Item.java
+++ src/main/java/tracker/Item.java
@@ -5,9 +5,15 @@ import java.util.Objects;
 public class Item {
 
     private String id;
+
     private String name;
 
     public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String id, String name) {
+        this.id = id;
         this.name = name;
     }
 
@@ -28,12 +34,20 @@ public class Item {
     }
 
     @Override
+    public String toString() {
+        return String.format("id: %s, name: %s", id, name);
+    }
+
+    @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Item item = (Item) o;
-        return Objects.equals(id, item.id) &&
-                Objects.equals(name, item.name);
+        return Objects.equals(id, item.id) && Objects.equals(name, item.name);
     }
 
     @Override
